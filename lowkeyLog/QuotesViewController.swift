21eff41a81bc502bfc89d8e216b237acaad1c29f
Carlin Wright <carlinswright@gmail.com>
2021-07-22 13:29:17 -0400

    did some stuff

--- lowkeyLog/QuotesViewController.swift
+++ lowkeyLog/QuotesViewController.swift
@@ -9,13 +9,25 @@ import UIKit
 
 class QuotesViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    let arrayOfQuotes = ["Make time to take care of yourself", "Manifest your dream life.", "Life doesn't have to be perfect to be beautiful.", "PROGRESS not perfection", "Our destiny is not written for us, it's written by us.", "Start where you are. Use what you have. Do what you can.", "The best revenge is massive success."]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        
+        
         // Do any additional setup after loading the view.
     }
     
 
+    @IBAction func buttonPressed(_ sender: Any) {
+        let randomNumber = Int.random(in: 0...(arrayOfQuotes.count-1))
+        label.text = arrayOfQuotes[randomNumber]
+        
+    }
     /*
     // MARK: - Navigation
 
